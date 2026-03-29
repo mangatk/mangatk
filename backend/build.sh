960515145968a17c528de23f936ac97bc937fcc2
athad-kun <amjdalshamy859@gmail.com>
2026-03-29 22:45:58 +0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
